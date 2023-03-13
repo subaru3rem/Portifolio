308ffe5ae1d9a0f7ea7ddeeaf0c750c41c39c494
subaru3rem <cainancosta6@gmail.com>
2023-03-13 14:07:45 -0300

    correção da ortografia

--- src/App.js
+++ src/App.js
@@ -21,12 +21,12 @@ function App() {
           Muito prazer, meu nome é Cainã Costa, sou um amante de tecnologia e um estudante full-stack,
           desde muito novo sou um amante de tecnologia sempre visando robótica como minha área de atuação,
           conforme a idade veio eu amadureci e minhas metas mudaram, fui desde a vontade de virar designer
-          até o ano de 2022 onde atravéz da instituição Nave do Conhecimento eu tive a oportunidade de
+          até o ano de 2022 onde através da instituição Nave do Conhecimento eu tive a oportunidade de
           aprender um pouco sobre montagem e manutenção onde ali eu tive o vislumbre do que seria minha
-          paixão reacender, minha paixão pela área foi renovada e ainda através dessa instituição eu
-          aprendi a progamar e tive diversas dicas e aprendizados na área que me trouxe ao momento em
+          paixão reacender, minha paixão pela área foi renovada e ainda através dessa instituição, eu
+          aprendi a programar e tive diversas dicas e aprendizados na área que me trouxe ao momento em
           que estou hoje, nesse site terá um pouco da minha caminhada e das minhas conquistas na área,
-          espero que gostem do que vêem e muito obrigado pela visita
+          espero que gostem do que vêem e muito obrigado pela visita.
           </p>
         </div>
       </section>
